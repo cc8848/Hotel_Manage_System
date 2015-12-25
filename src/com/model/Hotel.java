e0c08be8f996588dd9b4917db2e216c5d9a75477
gumingsiyi <stiles1024@qq.com>
2015-12-25 10:16:32 +0800

    添加了hotel类

---
+++ src/com/model/Hotel.java
@@ -0,0 +1,52 @@
+package com.model;
+
+/**
+ * Created by stiles on 15/12/25.
+ */
+public class Hotel {
+    private String id;
+    private String hotelName;
+    private int star;
+    private String address;
+    private String telephone;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getHotelName() {
+        return hotelName;
+    }
+
+    public void setHotelName(String hotelName) {
+        this.hotelName = hotelName;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
